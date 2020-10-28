5c7e56b3adf58fdad2d26380491dcbe89ac1b471
vicky-vargas <62673520+vicky-vargas@users.noreply.github.com>
2020-10-28 18:30:32 -0300

    Add files via upload

--- app/src/main/java/br/com/grupovellr/pointrestauranteepizzaria/TelaInicialActivity.kt
+++ app/src/main/java/br/com/grupovellr/pointrestauranteepizzaria/TelaInicialActivity.kt
@@ -1,23 +1,14 @@
 package br.com.grupovellr.pointrestauranteepizzaria
 
-import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
-import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_tela_inicial.*
-import kotlinx.android.synthetic.main.toolbar.*
+import kotlinx.android.synthetic.main.login.*
 
-class TelaInicialActivity : DebugActivity(), NavigationView.OnNavigationItemSelectedListener {
-
-    private val context: Context get() = this
-
+class TelaInicialActivity : DebugActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_inicial)
@@ -29,7 +20,11 @@ class TelaInicialActivity : DebugActivity(), NavigationView.OnNavigationItemSele
         val nome = args?.getString("nome_usuario")
         val numero = args?.getInt("numero")
 
+
         Toast.makeText(this, "Olá $nome", Toast.LENGTH_LONG).show()
+
+        supportActionBar?.title = "Menu"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         botaoPedido.setOnClickListener {
             val intent = Intent(this, AbrirComandaActivity::class.java)
@@ -45,57 +40,6 @@ class TelaInicialActivity : DebugActivity(), NavigationView.OnNavigationItemSele
             val intent = Intent(this, CardapioActivity::class.java)
             startActivity(intent)
         }
-
-        supportActionBar?.title = "Menu"
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        configuraMenulateral()
-    }
-
-    private fun configuraMenulateral() {
-        var toogle = ActionBarDrawerToggle(
-            this,
-            layoutMenuLateral,
-            toolbar_view,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        )
-        layoutMenuLateral.addDrawerListener(toogle)
-        toogle.syncState()
-
-        menu_lateral.setNavigationItemSelectedListener (this)
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId) {
-            R.id.nav_comanda -> {
-                val intent = Intent(this, AbrirComandaActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.nav_pedidos -> {
-                val intent = Intent(this, StatusPedidoActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.nav_cardapio -> {
-                val intent = Intent(this, CardapioActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.nav_adicionar -> {
-                val intent = Intent(this, AdicionarActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.nav_config -> {
-                val intent = Intent(this, ConfigActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.nav_sair -> {
-                val returnIntent = Intent();
-                returnIntent.putExtra("result","Saída do BrewerApp");
-                setResult(Activity.RESULT_OK,returnIntent);
-                finish();
-            }
-        }
-        layoutMenuLateral.closeDrawer(GravityCompat.START)
-        return true
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -106,23 +50,14 @@ class TelaInicialActivity : DebugActivity(), NavigationView.OnNavigationItemSele
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
 
-        when (id) {
-            R.id.action_config -> {
-                val intent = Intent(this, ConfigActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.action_menu_presenter -> {
-                val intent = Intent(this, AdicionarActivity::class.java)
-                startActivity(intent)
-            }
-
-            android.R.id.home-> {
-                if(layoutMenuLateral.isDrawerOpen(Gravity.LEFT))
-                    layoutMenuLateral.closeDrawer(Gravity.LEFT)
-                else
-                    layoutMenuLateral.openDrawer(Gravity.LEFT)
-            }
-
+        if (id == R.id.action_config) {
+            val intent = Intent(this, ConfigActivity::class.java)
+            startActivity(intent)
+        } else if (id == R.id.action_menu_presenter) {
+            val intent = Intent(this, AdicionarActivity::class.java)
+            startActivity(intent)
+        } else if (id == android.R.id.home) {
+            finish()
         }
 
         return super.onOptionsItemSelected(item)
